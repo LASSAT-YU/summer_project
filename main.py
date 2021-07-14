141c3eb3cf82d8cf4c1da7ea5b85a50624fdc527
Che <43485962+c-git@users.noreply.github.com>
2021-07-14 18:50:23 -0400

    Add files from Bazooka Bot

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+import os
+from threading import Thread
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import discord
+import flask
+from discord.ext import commands
+from waitress import serve
+
+from bot.custom_bot import Bot
+from conf import Conf
+from utils.db_cache import DBCache
+from utils.log import log, setup_logging
+from utils.repl_support import get_db
+
+#######################################################################
+"""
+Global Variables
+"""
+
+bot: Bot
+
+##############################################################################
+""" 
+    HTML Control Section
+    Had to put them in the same file to resolve issues with getting 
+    access to current tournament variable values
+
+"""
+web_interface = flask.Flask('Board')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@web_interface.route('/')
+def home():
+    return flask.render_template(
+            'board.html', content=bot.get_tournament_as_html())
 
 
-# Press the green button in the gutter to run the script.
+def run():
+    serve(web_interface, host="0.0.0.0", port=8080)
+
+
+def display_start():
+    Thread(target=run).start()
+
+
+##############################################################################
+
+
+def main():
+    global bot
+    log('Main Started')
+
+    bot = Bot(db=DBCache(get_db()),
+              command_prefix=commands.when_mentioned_or(Conf.COMMAND_PREFIX),
+              description=Conf.BOT_DESCRIPTION)
+
+    display_start()
+    bot.run(os.getenv(Conf.ENV.TOKEN))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    setup_logging(Conf.LOG_LEVEL)
+    main()
